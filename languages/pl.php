e50ef0a9a1c58fc34e4d01a9416c6634ff75e3a5
Marcin Cieslak <saper@saper.info>
2012-01-14 21:53:18 +0000

    Starting translation into Polish

---
+++ languages/pl.php
@@ -0,0 +1,117 @@
+<?php
+
+$messages['pl'] = array(
+		'header-title' => "Wikimania 2012 - Wniosek o stypendium",
+		'page-header' => "Wniosek o przyznanie stypendium na Wikimanię 2012",
+		'text-intro' => "
+<p>To jest wniosek o przyznanie stypendium do udziału w konferencji <a href='http://wikimania2012.wikimedia.org'>Wikimania 2012</a>, międzynarodowej
+konferencji ruchu Wikimedia. Osoby, którym zostanie przyznane stypendium, będą miały pokryty koszt rejestracji, noclegu i kosztów podróży na konferencję, która odbyła się w tym roku 12-15 lipca w Waszyngtonie.</p>
+<p>To stypendium <strong>nie jest</strong> stypendium naukowym bądź na podjęcie studiów. Wnioski nie związane z udziałem w konferencji będą odrzucone.</p>
+<p>Prosimy o rozważną ocenę swoich możliwości finansowych, gdyż środki na stypendia są ograniczone, a wielu ubiegających się nie ma w ogóle możliwości uczestniczenia w konferencji, jeśli nie otrzymają stypendium. Dziękujemy!</p>",
+		'form-errors' => '<p class="fieldWithErrors">Niektóre pola formularza nie zostały wypełnione właściwie. Prosimy poprawić informację w odpowiednich polach i spróbować wysłać formularz ponownie.</p>',
+		'contact-info' => "Dane kontaktowe",
+		'name-first' => "Imię:",
+		'name-last' => "Nazwisko:",
+		'form-email' => "Adres poczty elektronicznej:",
+		'form-telephone' => "Numer telefonu (prosimy podać kod kierunkowy kraju):",
+		'form-mailing-address' => "Adres korespondencyjny:",
+		'form-country-residence' => "Kraj zamieszkania: ",
+		'form-haspassport' => "Czy posiadasz ważny paszport?",
+		'form-yes' => "tak",
+		'form-no' => "nie",
+		'form-airport' => "Najbliższe lotnisko międzynarodowe:",
+		'form-personalinfo' => "Dane osobiste",
+		'form-select' => "Proszę wybrać",
+		'form-nationality' => "Narodowość - w jakim kraju został/będzie wydany Twój paszport?: ",
+		'form-language' => "Język ojczysty (lub języki): ",
+		'form-dateofbirth' => "Data urodzenia: ",
+		'form-gender' => "Płeć: ",
+		'form-gender-male' => "mężczyzna",
+		'form-gender-female' => "kobieta",
+		'form-gender-unspecified' => "nie chcę podawać",
+		'form-occupation' => "Zawód (jeśli pracujesz): ",
+		'form-study' => "Kierunek studiów (jeśli studiujesz): ",
+		'form-interest' => "Interest and involvement in Wikimania",
+		'form-attended' => "Czy w poprzednich latach brałeś lub brałaś udział w Wikimanii? Kiedy?",
+		'form-prior-recipient' => "Otrzymałem/otrzymałam poprzednio stypendium na wyjazd na Wikimanię",
+		'form-presenter' => "Zamierzam zgłosić (lub już zgłosiłam/zgłosiłem) propozycję wystąpienia na Wikimanii 2012 (<a href='http://wikimania2011.wikimedia.org/wiki/Call_for_Participation' target='_blank'>Zaproszenie do uczestnictwa</a>)",
+		'form-howheard' => "Skąd dowiedziałeś się lub dowiedziałaś się o możliwości starania się o stypendium?",
+		'form-howheard1' => "lista dyskusyjna / poczta elektroniczna",
+		'form-howheard2' => "strona jednego z projektów Wikimedia (na przykład strona Wikipedii)",
+		'form-howheard3' => "kawiarenka / bar / portal użytkowników jednego z projektów",
+		'form-howheard4' => "od znajomych",
+		'form-howheard5' => "w inny sposób",
+		'form-enrichment' => "Prosimy objaśnić, w jaki sposób Wikimania 2012 zyska na Twoim udziale",
+		'form-participation' => "Współpraca w ramach Wikimediów",
+		'form-username' => "Nazwa Twojego konta w projektach Wikimedia (lub na najważniejszym z nich): ",
+		'form-primary-project' => "Projektów, na którym najwięcej się udzielasz (Wikipedia, Wikimedia Commons, itp.): ",
+		'form-lang-version' => "Z której wersji językowej korzystasz najczęściej?",
+		'form-extent-explain' => "Opisz, w jaki sposób uczestniczysz w rozwoju projektów Wikimedia. Na przykład: należysz do stowarzyszenia wspierającego ruch Wikmediów, administratorem jednego z projektów, od dawna piszesz artykuły, tworzysz artykuły \"na medal\", często udostępniasz dobrej jakości zdjęcia lub grafiki, itp.",
+		'form-contrib-explain' => "Opisz, w jaki sposób uczestniczysz w innych niż Wikimedia projektach związanych z wolnym dostępem do wiedzy, wolnym oprogramowaniem czy też inicjatywach edukacyjnych.",
+		'form-future-explain' => "Opisz poniżej, w jaki sposób i dlaczego udział w konferencji Wikimania zmieni Twoje uczestnictwo w ruchu Wikimedia w przyszłości.",
+		'form-partial' => "Stypendium na pokrycie części kosztów",
+		'form-partial-explain' => "Oprócz pełnego pokrycia kosztów, mamy w tym roku możliwość udzielenia niewielkiej liczby <b>częściowych stypendiów</b>, które pozwalają na dofinansowanie kosztów dojazdu w wysokości do 300 dolarów amerykańskich.",
+		'form-wantspartial' => "Czy możemy brać Twoją kandydaturę pod uwagę przy udzielaniu częściowych stypendiów?",
+		'form-canpaydiff' => "Zobowiązuję się w przypadku otrzymania i przyjęcia stypendium na pokrycie części kosztów, że pokryję pozostałe koszty uczestnictwa w konferencji Wikimania 2012.",
+		'form-agree' => "Zobowiązanie osoby starającej się o stypendium",
+		'form-sincere' => "Oświadczam, że rozumiem w jakim celu składam niniejszy wniosek i mam szczery zamiar uczestniczyć w Wikimanii 2012 w Waszyngtonie. Zobowiązuję się użyć otrzymane środki zgodnie z przeznaczeniem, jeśli otrzymam i przyjmę stypendium.",
+		'form-visa' => "Zobowiązuję się uzyskać właściwą wizę z odpowiednim wyprzedzeniem (prosimy sprawdzić <a href='http://wikimania2012.wikimedia.org/wiki/Visas' target='_blank'>wymagania wizowe w przypadku podróży do Stanów Zjednoczonych</a>).",
+		'form-incidentals' => "Zobowiązuję się pokryć koszta dojazdu do i z lotniska, wyżywienia oraz inne własne wydatki podczas konferencji.",
+		'form-travel-conditions' => "Zobowiązuję się (a) skorzystać z biura podróży rekomendowanego przez Fundację Wikimedia w przypadku pokrycia bezpośredniego przez Fundację moich kosztów podróży albo (b) pokryć koszta podróży w cenie nie wyższej niż zaproponowana przez Fundację Wikimedia, a następnie przedstawić dokumenty potwierdzające poniesione koszty w celu otrzymania ich zwrotu.",
+		'form-review' => "Wnioski będą rozpatrywane przez członków komisji stypendialnej Wikimanii. Komisja stypendialna i Fundacja Wikipedia utrzymują wszystkie dane osobowe w ścisłej tajemnicy.",
+		'form-submit-app' => "Złóż wniosek",
+		'form-email-subject' => "Wikimania 2012 - wniosek o stypendium",
+		'form-email-response' =>
+"Witaj $1,
+
+Niniejszym potwierdzamy otrzymanie Twojego wniosku o pokrycie kosztów wyjazdu na konferencję
+Wikimania 2012. 
+
+Po zakończeniu oceny wszystkich wniosków poinformujemy o decyzji i
+We will let you know the decision on your scholarship after we finish reviewing and scoring all applications.
+
+Ewentualne pytania prosimy kierować na adres: wikimania-scholarships@wikimedia.org 
+
+Z pozdrowieniami,
+
+Komisja stypendialna Wikimanii",
+                'confirm-thanks' => "Serdecznie dziękujemy!",
+                'confirm-text' => "Dziękujemy za przesłanie wniosku o pokrycie kosztów wyjazd na konferencję Wikimania 2012. W razie ewentualnych pytań prosimy o kontakt pod adresem  <a href='mailto:wikimania-scholarships@wikimedia.org'>wikimania-scholarships@wikimedia.org</a>.",
+		'confirm-faq' => "<a href='http://wikimania2012.wikimedia.org/wiki/Scholarships/FAQ' target='_blank'>Odpowiedzi na zadawane pytania (FAQ)</a>",
+		'text-program-intro' => "<p>To jest wniosek o przyznanie stypendium do udziału w konferencji <a href='http://wikimania2012.wikimedia.org'>Wikimania 2012</a>, międzynarodowej
+konferencji ruchu Wikimedia. Osoby, którym zostanie przyznane stypendium, będą miały pokryty koszt rejestracji, noclegu i kosztów podróży na konferencję, która odbyła się w tym roku 12-15 lipca w Waszyngtonie.</p>
+<p>To stypendium <strong>nie jest</strong> stypendium naukowym bądź na podjęcie studiów. Wnioski nie związane z udziałem w konferencji będą odrzucone.</p>
+<p>Prosimy o rozważną ocenę swoich możliwości finansowych, gdyż środki na stypendia są ograniczone, a wielu ubiegających się nie ma w ogóle możliwości uczestniczenia w konferencji, jeśli nie otrzymają stypendium. Dziękujemy!</p>",
+		'text-have-submitted-presentation' => "Zgłosiłam/zgłosiłem propozycję prezentacji na Wikimanię 2012 (<a href='http://wikimania2011.wikimedia.org/wiki/Call_for_Participation' target='_blank'>Call for Participation</a>)",
+		'required-field' => "To pole musi być wypełnione",
+		'email-confirm-subject' => "Wikimania 2012 - wniosek o stypendium",
+		'email-confirm-text' =>
+"Niniejszym potwierdzamy otrzymanie Twojego wniosku o pokrycie kosztów wyjazdu na konferencję
+Wikimania 2012. 
+
+Ewentualne pytania prosimy kierować na adres:  ########################
+
+Z pozdrowieniami,
+
+Komisja stypendialna Wikimanii",
+		'mock' => "To jest strona do testów, składanie wnioski nie będą rozpatrywane.",
+		'email-from' => "Stypendia Wikimania 2012",
+		'scholarship-decision' => "Wikimania 2012 Scholarship Decision",
+		'new-account-subject' => "Nowe konto w programie stypendialnym Wikimania 2012",
+		'new-account-email' => "Utworzyliśmy dla Ciebie nowe konto w programie stypendialnym Wikimania 2012.
+
+Oto Twoje dane potrzebne do uzyskania dostępu:
+
+nazwa użytkownika: %s
+hasło:             %s
+
+Dostęp do informacji możesz uzyskać pod adresem: http://secure.wikidc.org/wm/schols/review/ 
+
+Możesz zmienić hasło korzystając z adresu:  https://secure.wikidc.org/wm/schols/review/user_pwreset.php
+
+Z poważaniem, 
+Katie Filbert
+Komitet organizacyjny konferencji Wikimania 2012"
+);
+
+?>
