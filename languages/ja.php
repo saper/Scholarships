2b32330af55b2e5b45e559715de74ed9f5331775
Ninomy <wiki.ninomy@gmail.com>
2012-02-02 16:19:56 +0900

    update ja.php

--- languages/ja.php
+++ languages/ja.php
@@ -1,42 +1,42 @@
 <?php
 
 $messages['ja'] = array(
-		'header-title' => "ウィキマニア2012 - スカラーシップ申し込み",
-		'page-header' => "ウィキマニア2012に参加するためのスカラーシップ申請",
+		'header-title' => "ウィキマニア2012 - 参加費補助申請",
+		'page-header' => "ウィキマニア2012参加のための費用補助申請",
 		'text-intro' => "
-<p>これは、ウィキメディアの国際会議「<a href='http://wikimania2012.wikimedia.org'>ウィキマニア2012</a>」への参加するための補助申請です。スカラーシップでは、2012年7月12日から15日までワシントンDCで開かれるウィキマニアへの参加費、宿泊費および交通費が支給されます。</p>
-<p>これは、大学での勉強に関する奨学金では<strong>ありません</strong>。明らかにウィキマニアに関連しない申し込みは無視されます。</p>
-<p>申し込みする方へ：本当に財政的補助が必要かどうか検討してください。ウィキマニアに参加するための補助に関する資金は限られており、たくさんの人たちが補助なしではウィキマニアに参加できません。ありがとうございます。</p>
-<p>申し込み期限は<strong>2012年2月16日</strong>です。</p>",
-		'form-error' => 'フォームに誤りがあります。誤りを修整し再度送信してください。',
+<p>こちらはウィキメディアの国際会議「<a href='http://wikimania2012.wikimedia.org'>ウィキマニア2012</a>」への参加費補助申請です。2012年7月12日から15日までアメリカ合衆国ワシントンD.C.において開催されるウィキマニアへの参加費、宿泊費および交通費が選考の上支給されます。</p>
+<p>これは、大学で勉強をするための奨学金では<strong>ありません</strong>。明らかにウィキマニアに関連しない申請は却下されます。</p>
+<p>申請される方へ：本当に財政的支援が必要かどうかをよく検討してください。ウィキマニア参加のための補助に支出できる資金は限られており、かつ補助なしではウィキマニアに参加することのできない方々が大勢いらっしゃいます。ありがとうございます。</p>
+<p>申請期限は<strong>2012年2月16日</strong>です。</p>",
+		'form-error' => '入力内容に誤りがあります。誤りを修正し再度送信してください。',
 		'contact-info' => "連絡先情報",
-		'name-first' => "名前（ファーストネーム）：",
-		'name-last' => "名字（ラストネーム）：",
-		'form-email' => "電子メールアドレス",
+		'name-first' => "名（ファーストネーム）：",
+		'name-last' => "姓（ラストネーム）",
+		'form-email' => "メールアドレス",
 		'form-telephone' => "電話番号（国番号を含む）：",
 		'form-mailing-address' => "住所：",
 		'form-country-residence' => "居住国： ",
-		'form-haspassport' => "現在パスポートを持っていますか？",
+		'form-haspassport' => "現在パスポートをお持ちですか？",
 		'form-yes' => "はい",
 		'form-no' => "いいえ",
 		'form-airport' => "最寄りの国際空港：",
 		'form-personalinfo' => "個人情報",
 		'form-select' => "選択してください",
-		'form-nationality' => "国籍（あなたのパスポートを発行するところ）： ",
+		'form-nationality' => "国籍（あなたのパスポートはどの国から発行されますか？）： ",
 		'form-passport-number' => "パスポート番号：",
-		'form-passport-explain' => "（これは、ビザが必要な場合にその申請を補助するのに必要です）",
+		'form-passport-explain' => "（ビザが必要な場合、その申請の補助に使用します）",
 		'form-language' => "第一言語（複数可）： ",
 		'form-dateofbirth' => "誕生日：",
 		'form-gender' => "性別：",
 		'form-gender-male' => "男",
 		'form-gender-female' => "女",
 		'form-gender-transgender' => "トランスジェンダー",
-		'form-gender-unspecified' => "どれにも該当しない",
+		'form-gender-unspecified' => "いずれにも該当しない",
 		'form-occupation' => "職種（専門家の場合）：",
-		'form-study' => "専門分野（学生の場合）：",
+		'form-study' => "専攻分野（学生の場合）：",
 		'form-interest' => "ウィキマニアへの関心と関与",
-		'form-attended' => "以前にウィキマニアに参加したことはありますか？ある場合、それは何年のウィキマニアでしたか？",
-		'form-prior-recipient' => "以前にもウィキマニアへの渡航補助・スカラーシップを受け取ったことがあります。",
+		'form-attended' => "以前ウィキマニアに参加したことはありますか？ある場合、それは何年のウィキマニアでしたか？",
+		'form-prior-recipient' => "以前にウィキマニアへの渡航補助・参加費補助を受け取ったことがあります。",
 		'form-presenter' => "ウィキマニア2012での発表に申し込みました/申し込む予定です（<a href='http://wikimania2012.wikimedia.org/wiki/Call_for_Participation' target='_blank'>発表申込</a>）",
 		'form-howheard' => "この申請をどこで知りましたか？",
 		'form-howheard1' => "メーリングリスト/電子メール",
@@ -44,54 +44,52 @@ $messages['ja'] = array(
 		'form-howheard3' => "井戸端/カフェ/コミュニティーポータル",
 		'form-howheard4' => "口コミ",
 		'form-howheard5' => "その他",
-		'form-enrichment' => "以下のスペースで、あなたがウィキマニア2012に参加することで、どのように会議が良くなるのかについて説明してください。",
+		'form-enrichment' => "下の入力欄で、あなたがウィキマニア2012に参加することで、この会議がどのように良くなるのか説明してください。",
 		'form-participation' => "ウィキメディアプロジェクトへの参加",
-		'form-username' => "最も参加しているウィキメディアプロジェクトでの利用者名：",
+		'form-username' => "最もよく参加しているウィキメディアプロジェクトでの利用者名：",
 		'form-primary-project' => "最も参加しているウィキメディア・プロジェクト（ウィキペディア、ウィキメディア・コモンズなど）：",
-		'form-lang-version' => "ウィキメディアのどの言語版を一番使用しますか？（複数可）",
-		'form-extent-explain' => "以下のスペースで、あなたがウィキメディアプロジェクトにどの範囲でどのような活動をしているのか説明してください（地域協会に所属していますか？管理者ですか？編集歴は長いですか？秀逸な記事の著者ですか？高品質な画像を投稿していますか？など）",
-		'form-contrib-explain' => "以下のスペースで、ウィキメディアプロジェクト以外の自由な知識共有やフリーソフトウェア、共同作業での教育的イニシアチブへの参加について説明してください。",
-		'form-future-explain' => "以下のスペースで、ウィキマニアへの参加が、どのように/何故あなたの将来のウィキメディア運動への関与を変えるのかについて説明してください。",
-		'form-partial' => "部分的スカラーシップ",
-		'form-partial-explain' => "完全スカラーシップに加え、今年は交通費の上限が300ユーロ（およそ380米ドル、3万円）の<b>部分的スカラーシップ</b>も選択可能です。",
-		'form-wantspartial' => "部分的スカラーシップにも申し込みますか？",
-		'form-canpaydiff' => "もし部分的スカラーシップが授与される場合、私はウィキマニア2012へ参加するための残りの費用を支払います。",
-		'form-agree' => "申し込みに関する同意",
-		'form-sincere' => "私はこの申込の目的を理解し、ワシントンDCでのウィキマニア2012への参加に本当に興味があります。補助を受け取ることができた場合、私はそれをこの目的のために使用します。",
-		'form-visa' => "補助を受け取ることができた場合、必要な場合、私は渡航ビザ（入国許可申請証）を旅行前に責任をもって取得します（アメリカ合衆国への旅行のための<a href='https://wikimania2012.wikimedia.org/wiki/Visas' alt='ビザ要件'>ビザ要件</a>も参照）。",
-		'form-incidentals' => "補助を受け取ることができた場合、私は会議中の空港への交通費、雑費、食費などを責任をもって支払います。",
-		'form-travel-conditions' => "補助を受け取ることができた場合、私はa)ウィキメディア財団が推奨する旅行予約サービスを使用し費用を直接支払う、b)自分自身でウィキメディア財団による費用見積り以下の費用で予約し補助を受け取るために領収書を提出する、のどちらかに同意します。",
+		'form-lang-version' => "ウィキメディアのどの言語版を最もよく使用しますか？（複数可）",
+		'form-extent-explain' => "下の入力欄で、あなたのウィキメディアプロジェクトにおける活動範囲とその内容を説明してください（地域協会に所属していますか？管理者ですか？編集歴は長いですか？秀逸な記事の著者ですか？高品質な画像を投稿していますか？など）",
+		'form-contrib-explain' => "下の入力欄で、自由な知識共有やフリーソフトウェア、共同作業あるいは教育的イニシアチブへのウィキメディアプロジェクト以外での参加について、その範囲と内容を説明してください。",
+		'form-future-explain' => "下の入力欄で、ウィキマニアへ参加することであなたの今後のウィキメディア運動への関わり方がどのようになぜ変わるのかを説明してください。",
+		'form-partial' => "減額支給",
+		'form-partial-explain' => "全額支給に加えて、今年は交通費の上限が300ユーロ（およそ380米ドル、3万円）の<b>減額支給</b>も選択可能です。",
+		'form-wantspartial' => "減額支給を合わせて申請しますか？",
+		'form-canpaydiff' => "減額支給が認められた場合、私はウィキマニア2012への参加に必要な残りの費用を支払います。",
+		'form-agree' => "申請の同意事項",
+		'form-sincere' => "私はこの申請の目的を理解しており、またアメリカ合衆国ワシントンD.C.で開催されるウィキマニア2012への参加に本当に興味があります。参加費補助の支給が認められた場合、私は支給された費用をこの目的のために使用します。",
+		'form-visa' => "参加費補助の支給が認められた場合、必要に応じて、私はビザ（査証）を渡航前に責任をもって取得します（アメリカ合衆国へ渡航するための<a href='https://wikimania2012.wikimedia.org/wiki/Visas' alt='ビザ要件'>ビザ要件</a>も参照）。",
+		'form-incidentals' => "参加費補助の支給が認められた場合、私は会場最寄りの空港から会場までの交通費、雑費および会期中の食費を責任をもって支払います。",
+		'form-travel-conditions' => "参加費補助の支給が認められた場合、私は a)ウィキメディア財団が推奨する旅行予約サービスを使用し、費用をウィキメディア財団から直接支払う、または、b)ウィキメディア財団による見積り以下の金額で自分自身で予約を行い、その領収書をウィキメディア財団に提出して領収書記載の金額の払い戻しを受ける、のいずれかに同意します。",
 		'form-submit-app' => "申請を送信",
-		'form-email-subject' => "ウィキマニア2012スカラーシップ申請",
+		'form-email-subject' => "ウィキマニア2012 参加費補助申請",
 		'form-email-response' =>
-"$1様
+"$1 様
 
-これは、ウィキマニア2012のスカラーシップ申請の確認メールです。
+これはウィキマニア2012参加費補助申請の確認メールです。
 
-あなたのスカラーシップに関する決定は、全ての申し込みの確認と点数付けが終了次第、通知します。
+あなたの参加費補助に関する決定は、全ての申請の確認と点数付けが終了し次第通知します。
 
-質問がある場合は、 wikimania-scholarships@wikimedia.org までご連絡ください。
+不明な点がある場合は、wikimania-scholarships@wikimedia.org までご連絡ください。
 
-ウィキマニアスカラーシップチーム",
+ウィキマニア 参加費補助チーム",
                 'confirm-thanks' => "ありがとうございました！",
                 'confirm-text' =>
-"ウィキマニア2012ウィキマニア2012のスカラーシップへお申し込みいただきありがとうございました。
-質問がございましたら<a href='mailto:wikimania-scholarships@wikimedia.org'>wikimania-scholarships@wikimedia.org</a>までご連絡ください。",
+"ウィキマニア2012 参加費補助申請にお申し込みいただきありがとうございました。不明な点がございましたら<a href='mailto:wikimania-scholarships@wikimedia.org'>wikimania-scholarships@wikimedia.org</a>までご連絡ください。",
 		'confirm-faq' => "<a href='http://wikimania2012.wikimedia.org/wiki/Scholarships/FAQ' target='_blank'>よくある質問と回答</a>",
-		'text-have-submitted-presentation' => "
-ウィキマニア2012の発表を送信しました（<a href='http://wikimania2012.wikimedia.org/wiki/Call_for_Participation' target='_blank'>発表申し込み</a>）",
-		'email-confirm-subject' => "ウィキマニア2012スカラーシップ申請",
+		'text-have-submitted-presentation' => "ウィキマニア2012での発表を送信しました（<a href='http://wikimania2012.wikimedia.org/wiki/Call_for_Participation' target='_blank'>発表申し込み</a>）",
+		'email-confirm-subject' => "ウィキマニア2012参加費補助申請",
 		'email-confirm-text' =>
-"これは、ウィキマニア2012のスカラーシップ申請の確認メールです。
+"これはウィキマニア2012参加費補助申請の確認メールです。
 
 追加の質問については ######################## までご連絡ください。
 
-ウィキマニアスカラーシップチーム",
-		'mock' => "これはスカラーシップ申請の模擬試験サイトです。試験のみに使用してください。",
-		'email-from' => "ウィキマニア2012スカラーシップ",
-		'scholarship-decision' => "ウィキマニア2012スカラーシップに関する決定",
-		'new-account-subject' => "ウィキマニアスカラーシップのアカウント",
-		'new-account-email' => "ウィキマニアスカラーシップのあなたの新しいアカウントが作成されました。
+ウィキマニア 参加費補助チーム",
+		'mock' => "これは参加費補助申請の模擬試験サイトです。テストのためのみに使用してください。",
+		'email-from' => "ウィキマニア2012 参加費補助申請",
+		'scholarship-decision' => "ウィキマニア2012 参加費補助に関する決定",
+		'new-account-subject' => "ウィキマニア参加費補助の新しいアカウント",
+		'new-account-email' => "ウィキマニア参加費補助の新しいアカウントが作成されました。
 
 ログイン情報：
 
@@ -100,28 +98,28 @@ $messages['ja'] = array(
 
 http://secure.wikidc.org/wm/schols/review/ からログインしてください。
 
-パスワードは https://secure.wikidc.org/wm/schols/review/user_pwreset.php で変更可能です。
+パスワードは https://secure.wikidc.org/wm/schols/review/user_pwreset.php で変更することができます。
 
 Katie Filbert
-ウィキマニア2012運営チーム",
-		'not-open' => "スカラーシップ申請はまだ開始されていません。",
-		'deadline-passed' => "<div>スカラーシップ申請の申込期限を過ぎました。</div>",
-		'required-field' => "必須事項",
+ウィキマニア2012 運営チーム",
+		'not-open' => "参加費補助申請の受付はまだ開始していません。",
+		'deadline-passed' => "<div>参加費補助の申請期限を過ぎました。</div>",
+		'required-field' => "必須",
 		'form-privacy' => "プライバシー",
-                'form-review' => "本申請に含まれるすべての情報は、ウィキマニアスカラーシッププログラム委員会の委員によって確認されます。すべての個人情報は、委員会、<a href='http://wikimediafoundation.org'>ウィキメディア財団</a>、<a href='http://wikimediadc.org'>ウィキメディア・コロンビア特別区</a>によって機密情報として扱われます。",
-		'form-chapteragree' => "<a href='http://meta.wikimedia.org/wiki/Wikimedia chapters' alt='Wikimedia chapters'>ウィキメディア地域協会</a>のスカラーシップも申請し、申請情報をスカラーシップ選考のために地域協会と共有することに同意します。",
-		'privacy-page' => "<p>すべての情報は、ウィキマニアスカラーシッププログラム委員会の委員によって確認されます。</p>
-<p>すべての個人情報は、委員会、<a href='http://wikimediafoundation.org'>ウィキメディア財団</a>、<a href='http://wikimediadc.org'>ウィキメディア・コロンビア特別区</a>によって機密情報として扱われます、ウィキメディア財団の<a href='http://wikimediafoundation.org/wiki/Privacy_policy'>プライバシーポリシー</a>とウィキメディア・コロンビア特別区の<a href='http://wikimediadc.org/wiki/Privacy_polcy'>個人情報</a>および<a href='http://wikimediadc.org/wiki/Record_retention_and_document_destruction_policy'>記録保持と文書破棄に関する方針</a>にそれぞれ従います。.</p>",
+                'form-review' => "本申請に含まれるすべての情報は、ウィキマニア参加費補助プログラム委員会の委員によって確認されます。すべての個人情報は、委員会、<a href='http://wikimediafoundation.org'>ウィキメディア財団</a>、<a href='http://wikimediadc.org'>ウィキメディア・コロンビア特別区</a>によって機密情報として厳重に扱われます。",
+		'form-chapteragree' => "<a href='http://meta.wikimedia.org/wiki/Wikimedia chapters' alt='Wikimedia chapters'>ウィキメディア地域協会</a>の参加費補助にも申請し、申請情報を参加費補助の選考のために地域協会と共有することに同意します。",
+		'privacy-page' => "<p>本申請に含まれるすべての情報は、ウィキマニア参加費補助プログラム委員会の委員によって確認されます。</p>
+<p>すべての個人情報は、委員会、<a href='http://wikimediafoundation.org'>ウィキメディア財団</a>、<a href='http://wikimediadc.org'>ウィキメディア・コロンビア特別区</a>によって機密情報として厳重に扱われます。情報の取り扱いは、ウィキメディア財団の<a href='http://wikimediafoundation.org/wiki/Privacy_policy'>プライバシーポリシー</a>、および、ウィキメディア・コロンビア特別区の<a href='http://wikimediadc.org/wiki/Privacy_polcy'>プライバシーポリシー</a>並びに<a href='http://wikimediadc.org/wiki/Record_retention_and_document_destruction_policy'>記録保持と文書破棄に関する方針</a>にそれぞれ従います。.</p>",
 		'contact-us' => "連絡先",
 		'contact-page' => "メール： wikimania-scholarships@wikimedia.org",
 		'credits' => "クレジットとライセンス",
-		'credits-page' => "<p>ウィキマニアスカラーシップソフトウェアはオープンソースで、<a href='http://www.gnu.org/copyleft/gpl.html'>GNU General Public License 3.0</a>で<a href='http://github.com/filbertkm/Scholarships'>入手可能</a>です。</p>
+		'credits-page' => "<p>ウィキマニア参加費補助ソフトウェアはオープンソースで、<a href='http://www.gnu.org/copyleft/gpl.html'>GNU General Public License 3.0</a>で<a href='http://github.com/filbertkm/Scholarships'>入手可能</a>です。</p>
 
 <p>コードはウィキマニア2009、2010、2011に使用されたスカラーシップ申請システムを基本に、ウィキマニア2012用に修正されたものです。</p>",
 		'help-translate' => "翻訳する",
-		'translate-page' => "<p>スカラーシップ申請システムをできるだけ多くの言語での翻訳を歓迎します。</p>
+		'translate-page' => "<p>参加費補助申請システムの多くの言語への翻訳を歓迎します。</p>
 
-<p>コードと言語ファイルは<a href='http://github.com/filbertkm/Scholarships'>github</a>にあり、そこで翻訳を送信してください。</p>
+<p>コードと言語ファイルは<a href='http://github.com/filbertkm/Scholarships'>github</a>にあります。そこで翻訳を送信してください。</p>
 
 <p>ありがとうございます！<br/>
 ウィキマニア2012運営チーム",
